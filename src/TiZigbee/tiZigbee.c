994066f4bc1fd67fcd0dc60275a3ce0b613d4e9c
lyj789 <lyj981014@163.com>
2021-04-21 11:20:59 -0400

    fix a little bug

--- src/TiZigbee/tiZigbee.c
+++ src/TiZigbee/tiZigbee.c
@@ -5,37 +5,28 @@
 #include "common.h"
 #include "VendorFunction.h"
 
-//bool gLightBulbState = false;
-//pthread_mutex_t gMutexLightBulb;
-
-
 void runZigbee(void){
-    // exit if this channel is not avaliable
-	if (checkChannel("Zigbee") == 0){
-		printf("Channel closed. \n");
-		return;
-	}
-
 	int fd;
 	int receiveVal;  
-	int end = 0;
 	if (wiringPiSetup() < 0)
 		return;
 	if ((fd = serialOpen("/dev/ttyS0", 115200)) < 0)
 		return;
 	serialPrintf(fd, "1");
-	
-	
-	
+		
 	printf("waiting... \n");
-	while(end==0)
+	while(1)
 	{  
-
-	  if(serialDataAvail(fd) > 0)
-	  {      
+		if (checkChannel("Zigbee") == 0){
+			printf("Channel closed. \n");
+			serialPrintf(fd, "0");
+			serialClose(fd);
+			return;
+		}	
+     
 		receiveVal=  serialGetchar(fd);
-		//printf("receiveVal: %d\n", receiveVal);	
-	
+		//printf("receiveVal: %d\n", receiveVal);
+
 		switch(receiveVal)
 		{
 			case 235:   //on-n
@@ -53,17 +44,9 @@ void runZigbee(void){
 	        case 232:  //query-e
 			  printf("received command:>>e---stop Zigbee Control\n");
 			  disableChannel("Zigbee");
-			  end =1;
-			  serialPrintf(fd, "0");
-			  serialClose(fd);
-			  break;
-			
-			//default:
-			//  printf("[Error:] Unsupported command.\n");
-			//  break;
-		}
-				
+			  break;				
 	  }
+	  
 	}
 	return;
 }
